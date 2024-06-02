42b4bbda822014fb60dfe3e29f882bc677757139
nyanishinde <nyanishinde10@gmail.com>
2024-06-02 20:57:26 +0530

    Added activity_add_contacts.xml and activity_view_contacts.xml to the projects and also designed the home page

--- app/src/main/java/com/example/sqlitecurd/MainActivity.java
+++ app/src/main/java/com/example/sqlitecurd/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.sqlitecurd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView addContact,viewContact;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addContact=findViewById(R.id.cardAddContact);
+        viewContact=findViewById(R.id.cardViewContact);
+
+        addContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddContacts.class));
+            }
+        });
+
+        viewContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ViewContacts.class));
+            }
+        });
 
     }
 }
