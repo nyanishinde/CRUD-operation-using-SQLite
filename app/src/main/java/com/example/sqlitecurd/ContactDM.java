5262c8b02da2701f4a18d6b225b421e305e519aa
nyanishinde <nyanishinde10@gmail.com>
2024-06-05 11:35:33 +0530

    Created data model class for ContactDM.java

---
+++ app/src/main/java/com/example/sqlitecurd/ContactDM.java
@@ -0,0 +1,45 @@
+package com.example.sqlitecurd;
+
+public class ContactDM {
+
+    String name,address,phone,email;
+
+    public ContactDM(String name, String address, String phone, String email) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
